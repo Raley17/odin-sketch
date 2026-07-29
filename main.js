6f9fb80480c2d0b24c0c6127d20686a2322c1fd4
Rama <ramirologiurato@gmail.comgit config --global user.name Ramagit statusgit config --global user.email ramirologiurato@gmail.com2>
2026-07-29 15:09:18 -0300

    Added those 'Extra Credit' features, project finished.

--- main.js
+++ main.js
@@ -17,9 +17,11 @@ newGridBtn.classList.add("gridBtn");
 newGridBtn.textContent = "New Grid";
 document.body.appendChild(newGridBtn);
 
-// New grid size prompt
+// Array with acceptable values
 
 const acceptableValues = [4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+// New grid size prompt
 
 function newGrid(value) {
   if (acceptableValues.includes(value)) {
@@ -36,10 +38,14 @@ function newGrid(value) {
   }
 }
 
+// New Grid Button
+
 newGridBtn.addEventListener("click", () => {
   const value = Number(prompt("Enter your new grid size"));
   newGrid(value);
 });
+
+// Text with acceptable values
 
 const possValues = document.createElement("p");
 possValues.style.marginBottom = "1rem";
@@ -48,3 +54,24 @@ possValues.textContent =
   "Enter one of these values 4, 9, 16, 25, 36, 49, 64, 81, 100.";
 possValues.classList.add("valueList");
 document.body.appendChild(possValues);
+
+// Random RGB function
+
+newCont.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("cube")) {
+    // Give me the counter, if doesn't exist, use 0
+    let count = Number(e.target.dataset.count) || 0;
+
+    if (count >= 10) return;
+
+    count++;
+    // Without this line the counter would be 0 everytime
+    e.target.dataset.count = count;
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    e.target.style.opacity = count * 0.1;
+  }
+});
