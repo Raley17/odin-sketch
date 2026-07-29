2bbf6f467086c34cf9e5cd28fb3329600403fd52
Rama <ramirologiurato@gmail.comgit config --global user.name Ramagit statusgit config --global user.email ramirologiurato@gmail.com2>
2026-07-28 22:26:58 -0300

    Logic added, project almost finished, tomorrow will begin 'Extra credit' tasks.

--- main.js
+++ main.js
@@ -19,20 +19,32 @@ document.body.appendChild(newGridBtn);
 
 // New grid size prompt
 
+const acceptableValues = [4, 9, 16, 25, 36, 49, 64, 81, 100];
+
 function newGrid(value) {
-  if (value % 4 === 0 && value < 100) {
+  if (acceptableValues.includes(value)) {
     newCont.innerHTML = "";
     for (let i = 1; i <= value; i++) {
       const createDiv = document.createElement("div");
       createDiv.classList.add("cube");
+      createDiv.style.width = `${32 / Math.sqrt(value)}rem`;
+      createDiv.style.height = `${32 / Math.sqrt(value)}rem`;
       newCont.appendChild(createDiv);
     }
   } else {
-    alert("Your new grid isn't / 4");
+    alert("You submitted a wrong value!");
   }
 }
 
 newGridBtn.addEventListener("click", () => {
-  const value = prompt("Enter your new grid size (Must be / by 4)");
+  const value = Number(prompt("Enter your new grid size"));
   newGrid(value);
 });
+
+const possValues = document.createElement("p");
+possValues.style.marginBottom = "1rem";
+possValues.style.color = "white";
+possValues.textContent =
+  "Enter one of these values 4, 9, 16, 25, 36, 49, 64, 81, 100.";
+possValues.classList.add("valueList");
+document.body.appendChild(possValues);
